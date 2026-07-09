1c346878a8c378cb699c2a382b14d9cf91821303
Ryukaaaz <michael2510.work@gmail.com>
2026-07-09 18:09:18 +0700

    Adding role verify for the routes

--- routes/web.php
+++ routes/web.php
@@ -9,29 +9,35 @@ use App\Http\Controllers\GradeController;
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    //staff
     Route::get('/dashboard', function () {
         return Inertia\Inertia::render('dashboard/index');
     })->name('dashboard');
 
-    //expansion set routes
-    Route::get('/expansion-sets',[ExpansionSetController::class, 'index'])->name('expansion.index');
 
 
-    //cards routes
-    Route::get('/cards/show/{expansionSetId}',[CardController::class,'index'])->name('cards.index')->whereNumber('expansionSetId');
-    //create new card
-    Route::get('/cards/create/{expansionSetId}',[CardController::class,'create'])->name('cards.create');
-    Route::post('/cards/store',[CardController::class,'store'])->name('cards.store');
-    //update new card
-    Route::get('/cards/edit/{cardId}',[CardController::class,'edit'])->name('cards.edit');
-    Route::patch('/cards/update/{cardId}',[CardController::class,'patch'])->name('cards.update');
+    //admin
+    Route::middleware('admin')->group(function () {
+        //expansion set routes
+        Route::get('/expansion-sets', [ExpansionSetController::class, 'index'])->name('expansion.index');
 
 
-    //rarity routes
-    Route::get('/rarity',[RarityController::class,'index'])->name('rarity.index');
+        //cards routes
+        Route::get('/cards/show/{expansionSetId}', [CardController::class, 'index'])->name('cards.index')->whereNumber('expansionSetId');
+        //create new card
+        Route::get('/cards/create/{expansionSetId}', [CardController::class, 'create'])->name('cards.create');
+        Route::post('/cards/store', [CardController::class, 'store'])->name('cards.store');
+        //update new card
+        Route::get('/cards/edit/{cardId}', [CardController::class, 'edit'])->name('cards.edit');
+        Route::patch('/cards/update/{cardId}', [CardController::class, 'patch'])->name('cards.update');
 
-    //grade routes
-    Route::get('/grade',[GradeController::class,'index'])->name('grade.index');
+
+        //rarity routes
+        Route::get('/rarity', [RarityController::class, 'index'])->name('rarity.index');
+
+        //grade routes
+        Route::get('/grade', [GradeController::class, 'index'])->name('grade.index');
+    });
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
